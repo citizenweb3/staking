f912bc4e278f616e4680c6bc0307b31ae6bbca6a
alibabaedge <49843222+alibabaedge@users.noreply.github.com>
2025-01-03 23:44:41 +0100

    Update header.tsx

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link';
-
 import Button from '@/app/components/common/button';
 
 const Header = () => (
-  <div className="flex flex-row justify-between">
+  <div className="flex flex-row justify-between items-center">
     <Link href={'/'} className="font-hisqaida">
       <span className="text-3xl">citizen</span>
       <span className={`text-lg`}> web3</span>
     </Link>
 
-    <Link href={`https://validatorinfo.com/`} target="_blank" rel="nofollow" className="text-base">
-      <Button external>Open App</Button>
-    </Link>
-    <Link href={``} target="_blank" rel="nofollow" className="text-base">
-      <Button external>Public Monitoring Page</Button>
-    </Link>
+    <div className="flex space-x-2"> {/* Добавляем flex контейнер для кнопок */}
+      <Link href={`https://validatorinfo.com/`} target="_blank" rel="nofollow" className="text-base">
+        <Button external>Open App</Button>
+      </Link>
+      <Link href={``} target="_blank" rel="nofollow" className="text-base">
+        <Button external>Public Monitoring Page</Button>
+      </Link>
+    </div>
   </div>
 );
 
