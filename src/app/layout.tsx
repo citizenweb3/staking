e2b0c6fa2625d4822c71a17d0199f4f043733a01
stribulsergey <ssargiuz@gmail.com>
2025-02-03 20:58:05 -0300

    added favicons and og info for socials

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,26 +55,41 @@ export const metadata: Metadata = {
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en" className={`${zonaPro.variable} ${hisqaida.variable}`}>
-      <head>
-        <title>Citizen Web3 staking page</title>
-        <script defer data-domain="staking.citizenweb3.com" src="https://plausible.io/js/script.js"></script>
-      </head>
-      <body className={`${zonaPro.className} scrollbar-track-scroll-bg scrollbar-thumb-scroll-item`}>
-        <div className="flex min-h-screen w-full flex-col justify-between bg-black px-8 py-4 text-white">
-          <Header />
-          <main className="flex-1 pt-10">{children}</main>
-          <Footer />
-        </div>
-        <ToastContainer
-          position="top-right"
-          autoClose={300}
-          hideProgressBar
-          closeOnClick
-          pauseOnHover
-          draggable
-          theme="dark"
-        />
-      </body>
+    <head>
+      <title>Stake with Citizen Web3 Validator. Our public goods, tools and infrastructure</title>
+      <script defer data-domain="staking.citizenweb3.com" src="https://plausible.io/js/script.js"></script>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Stake with Citizen Web3 Validator. Our public goods, tools and infrastructure" />
+      <meta property="og:description" content="Citizen Web3 - Non-Custodial, Self-Hosted, Bare-Metal Validator. Stake with us to earn rewards" />
+      <meta property="og:image" content="https://staking.citizenweb3.com/logo.png" />
+      <meta property="og:url" content="https://staking.citizenweb3.com" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Stake with Citizen Web3 Validator. Our public goods, tools and infrastructure" />
+      <meta name="twitter:description" content="Citizen Web3 - Non-Custodial, Self-Hosted, Bare-Metal Validator. Stake with us to earn rewards" />
+      <meta name="twitter:image" content="https://staking.citizenweb3.com/logo.png" />
+      <meta name="twitter:url" content="https://https://staking.citizenweb3.com" />
+    </head>
+    <body className={`${zonaPro.className} scrollbar-track-scroll-bg scrollbar-thumb-scroll-item`}>
+    <div className="flex min-h-screen w-full flex-col justify-between bg-black px-8 py-4 text-white">
+      <Header />
+      <main className="flex-1 pt-10">{children}</main>
+      <Footer />
+    </div>
+    <ToastContainer
+      position="top-right"
+      autoClose={300}
+      hideProgressBar
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="dark"
+    />
+    </body>
     </html>
   );
 };
