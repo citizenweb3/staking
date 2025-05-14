c274f186dd2807da5ce2581787e031747f8b2b7d
alibabaedge <49843222+alibabaedge@users.noreply.github.com>
2025-05-14 20:23:53 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,35 @@
 import { getRepoChains } from '@/app/actions/repos';
 import ChainCard from '@/app/components/chain-card';
+import { IChainConfig } from '@/types';
+import { FC } from 'react';
+
+interface Props {
+  chains: IChainConfig[]
+  category: string
+}
+const ChainList: FC<Props> = async ({chains, category}) => {
+  return (
+    <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+      {chains
+        .filter((chain) => chain.category === category)
+        .map((chain) => {
+          return <ChainCard key={chain.name} chain={chain} />;
+        })}
+    </div>
+  )
+}
 
 const Home = async () => {
-  const mainnet = await getRepoChains();
+  const chains = await getRepoChains();
+
   return (
     <div>
       <h2 className="text-3xl font-semibold">Mainnets</h2>
-      <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-        {mainnet
-          .filter((chain) => chain.type === 'mainnet')
-          .map((chain) => {
-            return <ChainCard key={chain.name} chain={chain} />;
-          })}
-      </div>
+      <ChainList chains={chains} category={'mainnet'} />
       <h2 className="mt-12 text-3xl font-semibold">Testnets</h2>
-      <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-        {mainnet
-          .filter((chain) => chain.type === 'testnet')
-          .map((chain) => {
-            return <ChainCard key={chain.name} chain={chain} />;
-          })}
-      </div>
+      <ChainList chains={chains} category={'testnet'} />
+      <h2 className="mt-12 text-3xl font-semibold">Only infra</h2>
+      <ChainList chains={chains} category={'infra'} />
     </div>
   );
 };
