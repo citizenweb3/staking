f412dcef151d852be3146bbfe3f4c89e683586bc
m1amgn <medvedev.mgn@gmail.com>
2025-09-05 19:24:25 +0700

    added cw3 logo; back button behavior ignores elements, such as peers, etc; added anchors to sections (testnets, etc); added link to validatorinfo.com/network in the logo menu

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,18 +1,34 @@
+import Image from 'next/image';
 import Link from 'next/link';
+
 import Button from '@/app/components/common/button';
 
 const Header = () => (
-  <div className="flex flex-row justify-between items-center">
+  <div className="flex flex-row items-center justify-between">
     <Link href={'/'} className="font-hisqaida">
-      <span className="text-3xl">citizen</span>
-      <span className={`text-lg`}> web3</span>
+      <div className="flex flex-row items-center">
+        <Image width={30} height={30} src={'/cw3logo.png'} alt={'Logo'} className="h-12 w-12" />
+        <div className="flex flex-row items-end">
+          <div className="text-3xl">citizen</div>
+          <div className={`text-lg`}> web3</div>
+        </div>
+      </div>
     </Link>
 
-    <div className="flex space-x-2"> {/* Добавляем flex контейнер для кнопок */}
-    <Link href={``} target="_blank" rel="nofollow" className="text-base">
+    <div className="flex space-x-2">
+      <Link href="/#mainnets" className="text-base">
+        <Button>Mainnets</Button>
+      </Link>
+      <Link href="/#testnets" className="text-base">
+        <Button>Testnets</Button>
+      </Link>
+      <Link href="/#infra" className="text-base">
+        <Button>Infra</Button>
+      </Link>
+      <Link href={``} target="_blank" rel="nofollow" className="text-base">
         <Button external>Monitor</Button>
       </Link>
-      <Link href={`https://validatorinfo.com/`} target="_blank" rel="nofollow" className="text-base">
+      <Link href={`https://validatorinfo.com/networks`} target="_blank" rel="nofollow" className="text-base">
         <Button external>Val Info</Button>
       </Link>
       <Link href={`https://www.citizenweb3.com/episodes`} target="_blank" rel="nofollow" className="text-base">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import { FC } from 'react';
+
 import { getRepoChains } from '@/app/actions/repos';
 import ChainCard from '@/app/components/chain-card';
 import { IChainConfig } from '@/types';
-import { FC } from 'react';
 
 interface Props {
-  chains: IChainConfig[]
-  category: string
+  chains: IChainConfig[];
+  category: string;
 }
-const ChainList: FC<Props> = async ({chains, category}) => {
+
+const ChainList: FC<Props> = async ({ chains, category }) => {
   return (
     <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
       {chains
@@ -16,19 +18,25 @@ const ChainList: FC<Props> = async ({chains, category}) => {
           return <ChainCard key={chain.name} chain={chain} />;
         })}
     </div>
-  )
-}
+  );
+};
 
 const Home = async () => {
   const chains = await getRepoChains();
 
   return (
     <div>
-      <h2 className="text-3xl font-semibold">Mainnets</h2>
+      <h2 id="mainnets" className="text-3xl font-semibold">
+        Mainnets
+      </h2>
       <ChainList chains={chains} category={'mainnet'} />
-      <h2 className="mt-12 text-3xl font-semibold">Testnets</h2>
+      <h2 id="testnets" className="mt-12 text-3xl font-semibold">
+        Testnets
+      </h2>
       <ChainList chains={chains} category={'testnet'} />
-      <h2 className="mt-12 text-3xl font-semibold">Only infra</h2>
+      <h2 id="infra" className="mt-12 text-3xl font-semibold">
+        Only infra
+      </h2>
       <ChainList chains={chains} category={'infra'} />
     </div>
   );
