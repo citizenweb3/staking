304272238582e651bd97b45c94e358109d0a7147
stribulsergey <ssargiuz@gmail.com>
2025-09-09 16:45:20 -0300

    refactor ChainList component to categorize chains and improve layout

--- src/app/components/chain-list/chain-list.tsx
+++ src/app/components/chain-list/chain-list.tsx
@@ -1,32 +1,38 @@
-import { FC } from 'react';
+import {FC} from 'react';
 
 import ChainCard from '@/app/components/chain-list/chain-card';
-import { Selected, applyFacetFilter } from '@/app/utils/chain-list/filters-utils';
-import type { IChainConfig } from '@/types';
+import {applyFacetFilter, Selected} from '@/app/utils/chain-list/filters-utils';
+import type {IChainConfig} from '@/types';
 
 type OwnProps = {
-  chains: IChainConfig[];
-  selected: Selected;
+    chains: IChainConfig[];
+    selected: Selected;
 };
 
-const ChainList: FC<OwnProps> = ({ chains, selected }) => {
-  const filtered = applyFacetFilter(chains, selected);
+const categories = ['Privacy', 'Networks', 'AI+DePin', 'DeFi'];
 
-  if (filtered.length === 0) {
-    return (
-      <div className="mt-4 flex min-h-[120px] items-center justify-center rounded-md border border-dashed p-6 text-sm text-white">
-        Can't find networks for combination of filters. Please clean filters or click All button.
-      </div>
-    );
-  }
+const ChainList: FC<OwnProps> = ({chains, selected}) => {
+    const filtered = applyFacetFilter(chains, selected);
 
-  return (
-    <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-      {filtered.map((chain) => (
-        <ChainCard key={chain.name} chain={chain} />
-      ))}
-    </div>
-  );
+    if (filtered.length === 0) {
+        return (
+            <div
+                className="mt-4 flex min-h-[120px] items-center justify-center rounded-md border border-dashed p-6 text-sm text-white">
+                Can't find networks for combination of filters. Please clean filters or click All button.
+            </div>
+        );
+    }
+
+    return categories.map((category) => (
+        <div key={category} className="mb-8">
+            <h1 className="mb-4 text-3xl font-bold">{category}</h1>
+            <div className="mt-4 grid grid-cols-1 gap-4 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                {filtered.filter(chain => (chain as any).category?.includes(category)).map((chain) => (
+                    <ChainCard key={chain.name} chain={chain}/>
+                ))}
+            </div>
+        </div>
+    ));
 };
 
 export default ChainList;
