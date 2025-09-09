c61d085ab80f3035199f4d2527eb96c53ae995db
stribulsergey <ssargiuz@gmail.com>
2025-09-09 16:22:02 -0300

    remove ping animation from chain card component

--- src/app/components/chain-list/chain-card.tsx
+++ src/app/components/chain-list/chain-card.tsx
@@ -17,9 +17,9 @@ const ChainCard: FC<OwnProps> = ({ chain }) => {
 
   return (
     <div className="flex w-full flex-row rounded-3xl bg-paper p-4 shadow transition-all duration-300 hover:brightness-150">
-      <div className="mb-2 flex items-start gap-2">
-        <div className="ml-auto h-1 w-1 animate-ping rounded-full bg-green" />
-      </div>
+      {/*<div className="mb-2 flex items-start gap-2">*/}
+      {/*  <div className="ml-auto h-1 w-1 animate-ping rounded-full bg-green" />*/}
+      {/*</div>*/}
       <Link href={`/chains/${chain.name}`} className="flex flex-col items-center">
         <Image src={chain.icon} alt={chain.title} width={100} height={100} className="h-24 w-24" />
         <div className="mt-4 text-center text-lg font-semibold">{chain.title}</div>
