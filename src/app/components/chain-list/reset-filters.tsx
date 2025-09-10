5a6cd75938bdd9bf224e344ba1b9314eb2832ed6
m1amgn <medvedev.mgn@gmail.com>
2025-09-10 18:32:01 +0700

    added reset filter button; added h1, h2 titles

---
+++ src/app/components/chain-list/reset-filters.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+
+const ResetFilters = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const onClickResetAll = () => {
+    router.replace(pathname);
+  };
+
+  return (
+    <div className="justify-self-end">
+      <button
+        onClick={onClickResetAll}
+        className="rounded-md border border-white bg-black px-3 py-1.5 text-sm font-semibold capitalize text-white transition hover:border-black hover:bg-white hover:text-black active:border-white active:bg-black active:text-white"
+      >
+        Reset Filters
+      </button>
+    </div>
+  );
+};
+
+export default ResetFilters;
