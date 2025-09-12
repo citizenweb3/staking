938ea75f7ac488a6c8e503084f8fcc4e051c58f9
m1amgn <medvedev.mgn@gmail.com>
2025-09-12 18:11:46 +0700

    added cw3 logo hover behavior

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -4,13 +4,19 @@ import Link from 'next/link';
 import Button from '@/app/components/common/button';
 
 const Header = () => (
-  <div className="flex flex-row items-center justify-between">
+  <div className="group flex flex-row items-center justify-between">
     <Link href={'/'} className="font-hisqaida">
       <div className="flex flex-row items-center">
-        <Image width={100} height={100} src={'/cw3logo.png'} alt={'Logo'} className="h-12 w-12" />
+        <Image
+          width={100}
+          height={100}
+          src={'/cw3logo.png'}
+          alt={'Logo'}
+          className="h-12 w-12 shadow transition-all duration-300 group-hover:scale-110 group-hover:brightness-150 group-active:scale-95 group-active:brightness-75"
+        />
         <div className="flex flex-row items-end">
-          <div className="text-3xl">citizen</div>
-          <div className={`text-lg`}> web3</div>
+          <div className="text-3xl transition-all duration-300 group-hover:text-4xl group-active:text-3xl">citizen</div>
+          <div className="text-lg transition-all duration-300 group-hover:text-2xl group-active:text-lg"> web3</div>
         </div>
       </div>
     </Link>
