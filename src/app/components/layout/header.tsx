ee64f5cbb14ab0c4f2973664156937a22edcfebc
m1amgn <medvedev.mgn@gmail.com>
2025-09-30 07:27:45 +0700

    added new icons

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -25,9 +25,6 @@ const Header = () => (
       <Link href={`https://monitor.citizenweb3.com`} target="_blank" rel="nofollow" className="text-base">
         <Button external>Monitor</Button>
       </Link>
-      <Link href={`https://validatorinfo.com`} target="_blank" rel="nofollow" className="text-base">
-        <Button external>Val Info</Button>
-      </Link>
       <Link href={`https://www.citizenweb3.com/episodes`} target="_blank" rel="nofollow" className="text-base">
         <Button external>Podcast</Button>
       </Link>
